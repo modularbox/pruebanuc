c1a9500ed82467b68841146a4463a2182203b833
modularbox  s.l <modularboxs.l@MacBook-Air-de-modularbox.local>
2024-02-22 12:44:10 +0100

    version 1

---
+++ luces.py
@@ -0,0 +1,59 @@
+import time 
+from PyDMXControl.controllers import OpenDMXController
+from fixture_model import FixtureModel
+dmx = OpenDMXController()
+light_fixture_model = FixtureModel('RGBW') 
+
+# Big square fixture model
+bsq_fixture_model = FixtureModel("DRGBWSEP")
+
+lights = []
+lights2 = [] 
+lights3  = []
+listLights = []
+
+custom_fixture = dmx.add_fixture(Custom,name="CustomFixture", start_channel=1, channels=500)
+bsq_fixture_model.setup_fixture(custom_fixture)
+print("EncenderLuces")
+custom_fixture.dim(255, 0, 1)
+custom_fixture.dim(255, 0, 2)
+custom_fixture.dim(255, 0, 3)
+custom_fixture.dim(255, 0, 4)
+custom_fixture.dim(255, 0, 7)
+custom_fixture.dim(255, 0, 12)
+custom_fixture.dim(255, 0, 17)
+custom_fixture.dim(255, 0, 22)
+custom_fixture.dim(255, 0, 29)
+
+def encender():
+    
+    custom_fixture.dim(255, 0, 1)
+    custom_fixture.dim(255, 0, 2)
+    custom_fixture.dim(255, 0, 3)
+    custom_fixture.dim(255, 0, 4)
+    custom_fixture.dim(255, 0, 7)
+    custom_fixture.dim(255, 0, 12)
+    custom_fixture.dim(255, 0, 17)
+    custom_fixture.dim(255, 0, 22)
+    custom_fixture.dim(255, 0, 29)
+
+encender()
+while True:
+    # print("gehvbhdsb")
+
+    time.sleep(3)
+print("LucesTermino")
+"""
+ FT232R USB UART:
+
+                  Product ID: 0x6001
+                  Vendor ID: 0x0403  (Future Technology Devices International Limited)
+                  Version: 6.00
+                  Serial Number: AL0409WG
+                  Speed: Up to 12 Mb/s
+                  Manufacturer: FTDI
+                  Location ID: 0x00113000 / 6
+                  Current Available (mA): 500
+                  Current Required (mA): 90
+                  Extra Operating Current (mA): 0
+"""
