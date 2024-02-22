59814c63f08c2c1ddc9360d2ea95f3e935c1d0df
modularbox  s.l <modularboxs.l@MacBook-Air-de-modularbox.local>
2024-02-22 18:22:06 +0100

    Version api luces

--- luces.py
+++ luces.py
@@ -1,8 +1,10 @@
 import time 
+import requests
 from PyDMXControl.controllers import OpenDMXController
 from PyDMXControl.profiles.Generic import Custom
 from fixture_model import FixtureModel
 from leer_json import cargar_luces_desde_json
+from luces_json import Luces
 # Cargar luces desde JSON
 luces = cargar_luces_desde_json()
 
@@ -20,17 +22,41 @@ light_fixture_model = FixtureModel('RGBW')
 bsq_fixture_model = FixtureModel("DRGBWSEP")
 custom_fixture = dmx.add_fixture(Custom,name="CustomFixture", start_channel=1, channels=500)
 bsq_fixture_model.setup_fixture(custom_fixture)
-
+guardar_configuracion = ''
 def encender_luz(channel):
     custom_fixture.dim(255, 0, channel - 1)
 def apagar_luz(channel):
     custom_fixture.dim(0, 0, channel - 1)
 
-for channel in luces.encender:
-    encender_luz(channel)
-
+def ciclo_luces(luces):
+    for channel in luces:
+        encender_luz(channel)
 while True:
-    time.sleep(3)
+
+    def get_light_state_from_api():
+        try:
+            response = requests.get("https://api.conectateriolobos.es/luces/ermita")
+        except requests.exceptions.ConnectionError:
+            return None
+
+        # If there is no command, return None
+        if response.status_code != 200:
+            return None
+        if guardar_configuracion == response:
+            return None
+        
+        guardar_configuracion = response
+        # If there is a command, return it
+        print(response)
+        print(response.json())
+        data = response.json()
+        luces = Luces(data.get('encender'), data.get('apagar'))
+        return luces
+    
+    if get_light_state_from_api() != None: 
+        ciclo_luces(luces.encender)
+
+    time.sleep(4)
 print("LucesTermino")
 """
  FT232R USB UART:
