c46a4d78a4799708624eade1c637796149b14a27
System Administrator <root@MacBook-Air-de-modularbox.local>
2024-03-01 22:30:03 +0100

    Version 14

--- p1.py
+++ p1.py
@@ -1,51 +1,21 @@
 import asyncio
 import socketio
-import threading
-import time
-# Llamada a los sockets
-sio = socketio.AsyncClient(logger=True, engineio_logger=True)
-# Programas corriendo
-programa_continuo_running = False
-programa_por_tiempo_running = False
-# Programar cada programa
-def programa_continuo_function():
-    global programa_continuo_running
-    while programa_continuo_running:
-        print('Ejecutando el programa continuo...')
-        time.sleep(3)
 
-def programa_por_tiempo_function():
-    global programa_por_tiempo_running
-    while programa_por_tiempo_running:
-        print('Ejecutando el programa por tiempo...')
-        time.sleep(300)  # 5 minutos de espera
+# Crear un cliente de socket.io
+sio = socketio.AsyncClient()
 
+# Evento que se activa cuando se establece la conexión con el servidor
 @sio.event
 async def connect():
-    print('Conectado al servidor')
-    sio.emit('mensaje', 'Hola desde Python')
+    print('Conectado al servidor de Socket.IO')
 
-@sio.on('mensaje')
-async def on_mensaje(data):
-    print('Mensaje del servidor programa_continuo luces:', data)
-
-@sio.on('programacontinuo')
-async def on_programacontinuo(data):
-    print("Dsjfnkjsdnfkdmfsklm")
-    print('I received a message!', data)
-# @sio.event
-# def programacontinuo(data):
-#     print('Mensaje del servidor programa_continuo luces:', data)
-
-@sio.on('programaportiempo2')
-async def on_programacontinuo(data):
-    print('Mensaje del servidor programa_por_tiempo luces:', data)
-
+# Evento que se activa cuando se recibe un mensaje del servidor
 @sio.event
-async def disconnect():
-    print('Desconectado del servidor')
+async def message(data):
+    print('Mensaje del servidor:', data)
 
 async def main():
+    # Conectar al servidor de Socket.IO
     await sio.connect('http://192.168.1.136:3005')
 
     # Mantener un bucle infinito para recibir mensajes continuamente
@@ -54,9 +24,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-    # thread_programa_continuo = threading.Thread(target=programa_continuo_function)
-    # thread_programa_continuo.start()
-    # sio.wait()
-    # thread_programa_por_tiempo = threading.Thread(target=programa_por_tiempo_function)
-    # thread_programa_por_tiempo.start()
