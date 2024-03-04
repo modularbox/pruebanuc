82e8e2b3e13b6a36e565477f9cf96b9b0381b63a
System Administrator <root@MacBook-Air-de-modularbox.local>
2024-03-04 11:43:29 +0100

    Version 25

--- p1.py
+++ p1.py
@@ -8,9 +8,8 @@ def connect():
     print('connection established')
 
 @sio.event
-def ermita(data):
+def csermita(data):
     print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
 
 @sio.event
 def disconnect():
