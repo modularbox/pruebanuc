10d74b0b70c22115fc5b337b4593a2dd15003ae3
System Administrator <root@MacBook-Air-de-modularbox.local>
2024-03-04 11:14:03 +0100

    Version 18

--- p1.py
+++ p1.py
@@ -1,26 +1,13 @@
+
 import asyncio
-import socketio
+import websockets
 
-# Crear un cliente de socket.io
-sio = socketio.AsyncClient()
+async def recibir_mensajes():
+    uri = 'ws://192.168.1.136:3005'
+    async with websockets.connect(uri) as websocket:
+        while True:
+            mensaje = await websocket.recv()
+            print(f"Mensaje recibido: {mensaje}")
 
-# Evento que se activa cuando se establece la conexión con el servidor
-@sio.event
-async def connect():
-    print('Conectado al servidor de Socket.IO')
-
-# Evento que se activa cuando se recibe un mensaje del servidor
-@sio.event
-async def message(data):
-    print('Mensaje del servidor:', data)
-
-async def main():
-    # Conectar al servidor de Socket.IO
-    await sio.connect('http://192.168.1.136:3005')
-
-    # Mantener un bucle infinito para recibir mensajes continuamente
-    while True:
-        await asyncio.sleep(1)  # Esperar un poco antes de continuar
-
-if __name__ == '__main__':
-    asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(recibir_mensajes())
