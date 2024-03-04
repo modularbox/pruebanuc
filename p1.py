0ed4155afb04ec20ab17113ecf1f65578411ca50
System Administrator <root@MacBook-Air-de-modularbox.local>
2024-03-04 18:32:09 +0100

    Version 45

--- p1.py
+++ p1.py
@@ -1,5 +1,6 @@
 
 import threading
+import concurrent.futures
 import time
 import socketio
 import sys
@@ -13,11 +14,33 @@ thread_programa_por_tiempo = False
 request_programa = {}
 request_programa_por_tiempo = {}
 # Obtener el primer argumento que sera el lugar donde estara la nuc
+t_programa = None
+t_programa_por_tiempo = None
 # Example: Correr programa python3 luces.py lugar
 lugar = 'garaje'
 if len(sys.argv) > 1:
     lugar = sys.argv[1]
     print("El valor del parámetro es:", lugar)
+
+# Ejecutar el programa
+def ejecutar_programa():
+    global thread_programa
+    global request_programa
+    print("Ejecutar programa")
+    while thread_programa:
+        print("Programa ejecutandose")
+        luces_sockets.init_luces(request_programa)
+        time.sleep(1)
+
+# Ejecutar el programa
+def ejecutar_programa_por_tiempo():
+    global thread_programa_por_tiempo
+    global request_programa_por_tiempo
+    print("Ejecutar programa por tiempo")
+    while thread_programa_por_tiempo:
+        print("Programa por tiempo")
+        luces_sockets.programa_por_tiempo(request_programa_por_tiempo)
+        time.sleep(1)
 
 # Función para programar la ejecución del programa después de 10 segundos
 def programa_ejecucion(request):
@@ -30,14 +53,13 @@ def programa_ejecucion(request):
         if not thread_programa:
             luces_sockets.off_all_channels()
             thread_programa = True
-            t_programa.start()
+            t_programa = iniciar_programa(ejecutar_programa)
             print("Peiridankjdnjn")
         else:
-            thread_programa = False
-            t_programa.join()
+            t_programa.cancel()
             luces_sockets.off_all_channels()
             thread_programa = True
-            t_programa.start()
+            t_programa = iniciar_programa(ejecutar_programa)
     
 # Función para programar la ejecución del programa después de 10 segundos
 def programa_por_tiempo_ejecucion(request):
@@ -48,42 +70,31 @@ def programa_por_tiempo_ejecucion(request):
     global t_programa_por_tiempo
     request_programa_por_tiempo = request
     if thread_programa:
-        thread_programa = False
-        t_programa.join()
+         thread_programa = False
+         t_programa.cancel()
     # Ejecutamos el programa en el tiempo especifico   
     if not thread_programa_por_tiempo:
         luces_sockets.off_all_channels()
-        thread_programa_por_tiempo = True
-        t_programa_por_tiempo.start()
+        t_programa_por_tiempo = iniciar_programa(ejecutar_programa_por_tiempo)
         time.sleep(request.get('time'))
-        thread_programa_por_tiempo = False
+        t_programa_por_tiempo.cancel()
         luces_sockets.off_all_channels()
-        t_programa_por_tiempo.join()
-        thread_programa = True
-        t_programa.start()
+        t_programa = iniciar_programa(ejecutar_programa)
    
-# Ejecutar el programa
-def ejecutar_programa():
-    global thread_programa
-    global request_programa
-    print("Ejecutar programa")
-    while thread_programa:
-        print("Programa ejecutandose")
-        luces_sockets.init_luces(request_programa)
-        time.sleep(2)
-
-# Ejecutar el programa
-def ejecutar_programa_por_tiempo():
-    global thread_programa_por_tiempo
-    global request_programa_por_tiempo
-    print("Ejecutar programa por tiempo")
-    while thread_programa_por_tiempo:
-        luces_sockets.programa_por_tiempo(request_programa_por_tiempo)
-        time.sleep(2)
+# Función para iniciar el programa
+def iniciar_programa(funcion):
+    print("Iniciando programa...")
+    # Iniciar el programa utilizando el executor
+    theared = concurrent.futures.ThreadPoolExecutor().submit(funcion)
+    return theared
 
 # Iniciar los hilos con programa por tiempo, y programa
-t_programa = threading.Thread(target=ejecutar_programa)
-t_programa_por_tiempo = threading.Thread(target=ejecutar_programa_por_tiempo)
+# t_programa = threading.Thread(target=ejecutar_programa)
+# Iniciar el programa utilizando el executor
+# t_programa = iniciar_programa(ejecutar_programa)
+# t_programa_por_tiempo = threading.Thread(target=ejecutar_programa_por_tiempo)
+# Iniciar el programa utilizando el executor
+# t_programa_por_tiempo = iniciar_programa(ejecutar_programa_por_tiempo)
 
 # Funcion de los sockets
 @sio.event
