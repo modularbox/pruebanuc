0d78955d6faf3be6a7e991418a4b74116e09539d
System Administrator <root@MacBook-Air-de-modularbox.local>
2024-03-04 13:09:57 +0100

    Version 31

--- p1.py
+++ p1.py
@@ -31,7 +31,7 @@ def connect():
 
 @sio.on('programa' + lugar)
 def programa(data):
-    programar_ejecucion(str(data).json())
+    programar_ejecucion(data)
 
 @sio.on('programa_por_tiempo' + lugar)
 def programa_por_tiempo(data):
